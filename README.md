8823e8eab04d1bd35b38a6646b03af5bcdf8b033
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2022-03-25 14:29:54 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# wp1101
+
+基本要求：
+
+1.完成整體TODOList框架
+
+2.輸入TODO item的文字，按下enter後就會新增項目
+
+3.當input box內為空時按下enter後不會有反應
+
+4.左下角display box同步更新顯示剩餘“未完成“TODO items的數量
+
+
+進階要求：
+
+1.只有在TODO item不為“0”時下方footer才會顯示
+
+2.實作All, Active, Completed, Clear Completed四個按鈕功能
+
+3.當已完成的TODO items不為 0 時，則Clear completed按鈕才會出現，且點擊它時，所有"已完成"TODO items 會被刪除
+
+4.在不同模式下All, Active, Completed其按鈕會有Highlight
+
+5.在Active模式下將“未完成”TODO item打勾，此“未完成”TODO item會消失，可以在All或Completed模式看到此TODO item
+
+
+
+
---
+++ main.js
@@ -0,0 +1,176 @@
+var total_item = 0;
+var index = 0;
+var todos = {};
+var mode = 0;
+var btn_list = [document.getElementById("one"), document.getElementById("two"), document.getElementById("three")];
+
+var item_box = document.getElementById("todo-list");
+var inp = document.getElementsByClassName("todo-app__input")[0];
+inp.placeholder = "What needs to be done?";
+inp.addEventListener("keydown", function(e){
+    if(e.code=="Enter"&&inp.value.trim()!==""){
+        total_item+=1;
+        index+=1;
+        item_box.appendChild(new item(inp.value.trim(), index).item_node); 
+        inp.value = "";
+        footer_control();
+        show_clear_completed_btn();
+        if(mode==2){
+            show_completed();
+        }
+    }
+})
+
+class item {
+    constructor(todo, index){
+        //item
+        this.li = document.createElement("li");
+        this.li.classList.add("todo-app__item");
+        this.li.setAttribute("idx", index);
+
+        //left
+        let check_box = document.createElement("div");
+        check_box.classList.add("todo-app__checkbox");
+        let inp = document.createElement("input");
+        inp.id = index;
+        inp.type = "checkbox";
+        inp.addEventListener("click", function(e){
+            let count_screen = document.getElementsByClassName("todo-app__total");
+            count_screen[0].innerHTML = `${count_left()} left`;
+            if(e.target.checked){
+                e.target.parentNode.nextSibling.classList.add("done");
+            }else{
+                e.target.parentNode.nextSibling.classList.remove("done");
+            }
+            if(mode==1){
+                show_active();
+            }else if(mode==2){
+                show_completed();
+            }
+            show_clear_completed_btn();
+        })
+        let lab = document.createElement("label");
+        lab.setAttribute("for", index);
+        check_box.appendChild(inp);
+        check_box.appendChild(lab);
+    
+        //mid
+        let item_detail = document.createElement("h1");
+        item_detail.className = "todo-app__item-detail";
+        item_detail.innerHTML = todo;
+
+        //right
+        let img = document.createElement("img");
+        img.className = "todo-app__item-x";
+        img.src = "./img/x.png";
+        img.addEventListener("click", function(e){
+            let Pnode = e.target.parentNode;
+            delete todos[`${Pnode.getAttribute("idx")}`];
+            Pnode.remove();
+            total_item-=1;
+            footer_control();
+            show_clear_completed_btn();
+        })
+
+        //append
+        this.li.appendChild(check_box);
+        this.li.appendChild(item_detail);
+        this.li.appendChild(img);
+
+        //todos
+        todos[`${index}`] = this.li;
+    }
+
+    get item_node(){
+        return this.li;
+    }
+}
+
+function footer_control(){
+    let footer = document.getElementById("todo-footer");
+    if(total_item){
+        footer.style.display = "flex";
+    }else{
+        footer.style.display = "none";
+    }
+    let count_screen = document.getElementsByClassName("todo-app__total");
+    count_screen[0].innerHTML = `${count_left()} left`;
+}
+
+function count_left(){
+    let left_item = 0;
+    for([key, value] of Object.entries(todos)){    
+        if(!value.firstChild.firstChild.checked){
+            left_item++;
+        }
+    }
+    return left_item;
+}
+
+function show_all(){
+    mode = 0;
+    btn_highlight(mode);
+    for([key, value] of Object.entries(todos)){    
+        value.style.display = "flex";
+    }
+}
+
+function show_active(){
+    mode = 1;
+    btn_highlight(mode);
+    for([key, value] of Object.entries(todos)){ 
+        if(!value.firstChild.firstChild.checked){
+            value.style.display = "flex";
+        }else{
+            value.style.display = "none";
+        }
+    }
+}
+
+function show_completed(){
+    mode = 2;
+    btn_highlight(mode);
+    for([key, value] of Object.entries(todos)){ 
+        if(value.firstChild.firstChild.checked){
+            value.style.display = "flex";
+        }else{
+            value.style.display = "none";
+        }
+    }
+}
+
+function clear_completed(){
+    for([key, value] of Object.entries(todos)){ 
+        if(value.firstChild.firstChild.checked){
+            delete todos[`${value.getAttribute("idx")}`];
+            value.remove();
+            total_item-=1;
+        }
+    }
+    footer_control();
+    show_clear_completed_btn();
+}
+
+function show_clear_completed_btn(){
+    let clean = document.getElementById("clean")
+    if(total_item==count_left()){
+        clean.style.visibility = "hidden";
+    }else{
+        clean.style.visibility = "visible";
+    }
+}
+
+function btn_highlight(N){
+    for(i=0;i<=2;i++){
+        if(i==N){
+            btn_list[i].style.outline = "solid red"; 
+        }else{
+            btn_list[i].style.outline = "0px"; 
+        }
+    }
+}
+
+footer_control();
+btn_highlight(mode);
+
+
